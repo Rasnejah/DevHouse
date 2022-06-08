2142378624e556c3c95276299da4c9d96e273f7f
Renato da Silva Batista <renatososmicro@gmail.com>
2022-06-08 16:26:50 -0300

    include house on mongoDB

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,11 +1,44 @@
+import { json } from "express/lib/response"
+import House from "../models/House"
+import User from "../models/User"
+
 
 class HouseControler {
 
   async store(req, res){
 
-    return res.json({ ok: "house controler" })
+    const { filename } = req.file
+    const { description, price, location, status} = req.body
+    const { user_id } = req.headers 
+    
+    let user = await User.findOne({ _id: user_id})
+    if(!user){
+      return res.json({ massage: "Usuário não cadastrado"})
+    }
+    
+    try {
+
+      const house = await House.create({
+
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+
+      })
+
+      return res.json(house)
+      
+    } catch (err) {
+
+      return res.json(err.massage)
+
+    }
   }
 
 }
 
 export default new HouseControler()
+
