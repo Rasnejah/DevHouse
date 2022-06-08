a635fb8dc32995fe1f1ec4368d7d6241956663cf
Renato da Silva Batista <renatososmicro@gmail.com>
2022-06-08 14:28:06 -0300

    create model House

---
+++ src/models/House.js
@@ -0,0 +1,19 @@
+
+import { Schema, model } from "mongoose";
+import User from "./User";
+
+const HouseSchema = new Schema({
+
+  thumbnail:   String,
+  description: String,
+  price:       Number,
+  location:    String,
+  status:      Boolean,
+  user: {
+    type: Schema.Types.ObjectId,
+  ref:  'User'
+  }
+
+})
+
+export default model( 'House', HouseSchema )
