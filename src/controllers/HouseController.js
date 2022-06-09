600ccd1e3214c5b7190e4f13154e700f52990404
Renato da Silva Batista <renatososmicro@gmail.com>
2022-06-09 12:11:19 -0300

    create route /houses/:id and HouseController's metode update

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -60,6 +60,52 @@ class HouseControler {
     }
   }
 
+  async update (req, res){
+
+    const { house_id } = req.params
+    
+    try {
+
+      const house = await (await House.findOne({ _id: house_id }).distinct( 'user' )).toString()
+      console.log(house)
+
+      if( !house ){
+
+        res.status( 422 ).json({ massage: "Casa não encontrada"})
+
+      }
+
+      const { filename } = req.file
+      const { description, price, location, status} = req.body
+      const { user_id } = req.headers      
+
+      const user = await (await User.findById({ _id: user_id }).distinct( '_id' )).toString()
+      
+      if( user !== house ){
+        return res.status( 422 ).json({ massage: "Você não tem permição para auterar essa casa!"})
+      }
+
+      await House.updateOne( { _id: house_id }, {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      })
+
+      return res.json({ massage: "casa atualizada!"})
+
+    } catch (error) {
+
+      return res.status( 500 ).json( error.massage )
+
+    }
+    
+    
+    
+  }
+
 }
 
 export default new HouseControler()
