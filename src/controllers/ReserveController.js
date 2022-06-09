7765f5c44bb38cec5080bd0e0f23d3bf003da2f1
Renato da Silva Batista <renatososmicro@gmail.com>
2022-06-09 16:48:08 -0300

    create store metodo of ReserveController

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -6,7 +6,51 @@ class ReserveController{
 
   async store ( req, res ){
 
-    res.json({ ok: true })
+    const { user_id } = req.headers
+    const { date, house_id } = req.body
+    if( date === "" ){
+
+      return res.status( 422 ).json({ message: "preencha o campo data!"})
+
+    }
+
+    if( house_id === ""){
+
+      return res.status( 422 ).json({ message: "preencha o campo casa!"})
+
+    }
+
+    try {
+
+      const house = await await House.findById( house_id )
+      
+      if( String(house.user) === user_id ){
+
+        return res.status( 422 ).json({ message: "Reserva não autorizada!"})
+
+      }
+
+      if( !house.status ){
+
+        return res.status( 422 ).json({ message: "Casa não disponivel!"})
+        
+      }
+
+      const reserve = await Reserve.create({
+        date,
+        user: user_id,
+        house: house_id
+      })
+
+      await House.updateOne( reserve )
+
+      return res.json({ houseUser_id: house.user, user_id})
+
+    } catch (error) {
+      
+      return res.status( 500 ).json(error.message)
+
+    }    
 
   }
 
